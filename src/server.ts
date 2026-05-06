d2b50103877ee550d0605077b6f5641e360f6cec
vitorsonn <vitorson96@gmail.com>
2026-05-06 14:20:14 -0300

    fix deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import app from "./app.js"
+import app from "./app.js";
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () =>{
-    console.log("Servidor funfando")
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
